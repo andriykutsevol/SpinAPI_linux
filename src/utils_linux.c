897d049d5ec5541d32a24020b2fa05d989aced67
Andriy Kutsevol <andriykutsevol@gmail.com>
2022-05-27 15:21:28 +0300

    c

--- src/utils_linux.c
+++ src/utils_linux.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "utils_linux.h"
 
-void pci_get_resource0(int dev_id, char* resource0_path){
+void pci_get_resource0(int dev_id, char *resource0_path){
 
-  resource0_path = "pci_get_resource0";
+  strcpy(resource0_path, "pci_get_resource0");
 
 }
