923f85d72aed27af6ec1a3c8659d242cc0c2c44d
Andriy Kutsevol <andriykutsevol@gmail.com>
2022-05-27 17:14:43 +0300

    c

--- src/utils_linux.c
+++ src/utils_linux.c
@@ -40,7 +40,7 @@ int pci_get_resource0(int dev_id, char *resource0_path){
     fgets (number_s, 16, fd); 
     fclose(fd);
 
-    printf("number_s: %s\n", number_s);
+    printf("number_s: %x\n", atoi(number_s));
 
     // if (atoi(&number) != address)
     //   continue;    
