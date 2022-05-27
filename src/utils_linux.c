d8ca9183fe7251132d8f3b09440bb0cbbee8899d
Andriy Kutsevol <andriykutsevol@gmail.com>
2022-05-27 20:10:32 +0300

    c

--- src/utils_linux.c
+++ src/utils_linux.c
@@ -8,6 +8,69 @@
 
 
 
+// char* concat(const char *s1, const char *s2)
+// {
+//     char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+//     // in real code you would check for errors in malloc here
+//     strcpy(result, s1);
+//     strcat(result, s2);
+//     return result;
+// }
+
+
+// // If you did happen to be bothered by performance then you would want to avoid repeatedly scanning the 
+// // input buffers looking for the null-terminator.
+
+// char* concat(const char *s1, const char *s2)
+// {
+//     const size_t len1 = strlen(s1);
+//     const size_t len2 = strlen(s2);
+//     char *result = malloc(len1 + len2 + 1); // +1 for the null-terminator
+//     // in real code you would check for errors in malloc here
+//     memcpy(result, s1, len1);
+//     memcpy(result + len1, s2, len2 + 1); // +1 to copy the null-terminator
+//     return result;
+// }
+
+
+
+// We can write a useful variadic function to concatenate any number of strings:
+#include <stdlib.h>       // calloc
+#include <stdarg.h>       // va_*
+#include <string.h>       // strlen, strcpy
+
+char* concat(int count, ...)
+{
+    va_list ap;
+    int i;
+
+    // Find required length to store merged string
+    int len = 1; // room for NULL
+    va_start(ap, count);
+    for(i=0 ; i<count ; i++)
+        len += strlen(va_arg(ap, char*));
+    va_end(ap);
+
+    // Allocate memory to concat strings
+    char *merged = calloc(sizeof(char),len);
+    int null_pos = 0;
+
+    // Actually concatenate strings
+    va_start(ap, count);
+    for(i=0 ; i<count ; i++)
+    {
+        char *s = va_arg(ap, char*);
+        strcpy(merged+null_pos, s);
+        null_pos += strlen(s);
+    }
+    va_end(ap);
+
+    return merged;
+}
+
+
+
+
 char *strremove(char *str, const char *sub) {
     size_t len = strlen(sub);
     if (len > 0) {
@@ -68,10 +131,12 @@ void find_resource0_listdir(const char *pci_sysdir, int dev_id, const char *top_
 
                 printf("path_to_pci_device: %s\n", path_to_pci_device);
 
-                char path_to_resource0[1024];
-                
-                strcat(path_to_resource0, path_to_pci_device);
-                strcat(path_to_resource0, "/resource0");
+                char *path_to_resource0 = concat(2, path_to_pci_device, "/resource0");
+
+
+                // char path_to_resource0[1024];
+                // strcat(path_to_resource0, path_to_pci_device);
+                // strcat(path_to_resource0, "/resource0");
 
                 printf("path_to_resource0: %s\n", path_to_resource0);
 
