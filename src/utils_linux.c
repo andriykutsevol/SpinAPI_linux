0fabfe087fcb99244fa4ed5b0958cb32dbea110d
Andriy Kutsevol <andriykutsevol@gmail.com>
2022-05-28 19:46:35 +0300

    c

--- src/utils_linux.c
+++ src/utils_linux.c
@@ -213,7 +213,7 @@ int pci_get_firmwareid(const char *resource0_path, int address, int *fw_result){
 
     printf("Value at offset 0x%X (%p): 0x%0*lX\n", (int) target + 0*type_width, virt_addr, type_width*2, read_result);
 
-    printf("read_result: %x", read_result);
+    printf("read_result: %lX\n", read_result);
 
     *fw_result = 1234;
 
