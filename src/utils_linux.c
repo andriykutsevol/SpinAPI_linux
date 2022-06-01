919e32ca622a6c10bea2a7e645be5f444f3d6713
Andriy Kutsevol <andriykutsevol@gmail.com>
2022-06-01 22:14:23 +0300

    c

--- src/utils_linux.c
+++ src/utils_linux.c
@@ -215,8 +215,8 @@ int mmap_outw(const char *resource0_path, int address, unsigned int data){
     virt_addr = map_base + target - target_base;
     printf("sofsafe: mmap_outw(): resource0_path: %s\n",  resource0_path);
     printf("sofsafe: mmap_outw(): target_base: %x\n",  target_base);
-    printf("sofsafe: mmap_outw(): map_base: %x\n",  map_base);
-    printf("sofsafe: mmap_outw(): virt_addr: %x\n", virt_addr);
+    printf("sofsafe: mmap_outw(): map_base: %x\n",  *(uint32_t *)map_base);
+    printf("sofsafe: mmap_outw(): virt_addr: %x\n", *(uint32_t *)virt_addr);  
     
     *((uint32_t *) virt_addr) = data;
 
