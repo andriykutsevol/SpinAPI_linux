db1851fc01238eb489791b683e3c965eb34dcafc
Andriy Kutsevol <andriykutsevol@gmail.com>
2022-06-02 17:17:26 +0300

    c

--- src/utils_linux.c
+++ src/utils_linux.c
@@ -135,7 +135,7 @@ int pci_get_resource0(int dev_id, char *result){
 
 
 
-int get_mmap_map_base(const char *resource0_path, 
+int get_mmap_virt_addr(const char *resource0_path, 
                         int address, 
                         void *virt_addr, 
                         void *map_base, 
@@ -185,7 +185,7 @@ int mmap_inw(const char *resource0_path, int address, int *fw_result){
     const int type_width = 4;
     void *map_base;
 
-    get_mmap_map_base(resource0_path, address, virt_addr, map_base, map_size);
+    get_mmap_virt_addr(resource0_path, address, virt_addr, map_base, map_size);
 
     read_result = *((uint32_t *) virt_addr);
 
--- src/utils_linux.h
+++ src/utils_linux.h
@@ -1,6 +1,6 @@
 int pci_get_resource0(int dev_id, char* resource0_path);
 
-int get_mmap_map_base(const char *resource0_path, 
+int get_mmap_virt_addr(const char *resource0_path, 
                         int address, 
                         void *virt_addr, 
                         void *map_base, 
