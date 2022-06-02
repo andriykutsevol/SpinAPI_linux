4a652bddb714ceb50de96f4a78cb12534afaf8a1
Andriy Kutsevol <andriykutsevol@gmail.com>
2022-06-02 17:33:44 +0300

    c

--- src/utils_linux.h
+++ src/utils_linux.h
@@ -2,7 +2,7 @@ int pci_get_resource0(int dev_id, char* resource0_path);
 
 int get_mmap_virt_addr(const char *resource0_path, 
                         int address, 
-                        void *virt_addr, 
+                        void **virt_addr, 
                         void *map_base, 
                         const int map_size);
 
