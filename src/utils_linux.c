88471f01b0cff4b1abe968df12ba8b24328719a8
Andriy Kutsevol <andriykutsevol@gmail.com>
2022-07-13 19:25:41 +0300

    cleanup

--- src/utils_linux.c
+++ src/utils_linux.c
@@ -59,16 +59,11 @@ char *strremove(char *str, const char *sub) {
 
 
 int if_string_in_array(char* str, char* array, int asize, int ssize){
-
-    printf("asize: %d\n", asize);
-    printf("str: %s\n", str);
     for(int i=0; i<asize; i=i+1){
-        printf("&array[ssize*%d]: %s\n", i, &array[ssize*i]);
         if(strcmp(str, &array[ssize*i]) == 0){       // 0 if strings are equal
             return 1;
         }
     }
-
     return 0;
 }
 
