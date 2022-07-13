cab391d84c3c5b6cfcea90241c358cf8011ea7d4
Andriy Kutsevol <andriykutsevol@gmail.com>
2022-07-13 19:25:16 +0300

    cleanup

--- src/utils_linux.c
+++ src/utils_linux.c
@@ -62,16 +62,13 @@ int if_string_in_array(char* str, char* array, int asize, int ssize){
 
     printf("asize: %d\n", asize);
     printf("str: %s\n", str);
-    int flg = 0;
     for(int i=0; i<asize; i=i+1){
         printf("&array[ssize*%d]: %s\n", i, &array[ssize*i]);
         if(strcmp(str, &array[ssize*i]) == 0){       // 0 if strings are equal
-            flg = flg + 1;
+            return 1;
         }
     }
-    if (flg >= asize){
-        return 1;
-    }
+
     return 0;
 }
 
